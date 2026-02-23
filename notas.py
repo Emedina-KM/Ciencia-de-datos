ff108921e412c359fb1ef1def22e645fd152b23c
emedina <emedina112233@gmail.com>
2026-02-22 20:48:05 -0500

    inicio de proyecto

---
+++ notas.py
@@ -0,0 +1,24 @@
+# Control de Versiones
+
+## GIT
+
+'''Objetivos principales
+
+1. Descemtralización.
+es que cada desarrollador tiene una copia del proyecto y del historico que se esta realizando. esto le facilita el trabajo sin conexión.
+2. Velocidad
+Todas las operaciones locales como los comentarios, ramas. Todo eso debia ser una gestión rapida. ASpi podias trabajar de manera local sin tener que depender del internet.
+3. Manejo eficiente de ramas
+muchos colaboradores trabajan a la vez un mismo proyecto.
+cada uno trabaja con una copia difernete de manera local.
+una vez terminan podran subir sus cambios al servidor es decir a su carpeta principal. pero antes de subirse para por un periodo de revisión
+si uno subio los cambios antes que el otro partiendo de la misma copia base, al segundo no lo dejara subir sus cambios sin antes descargar la última versión.
+así se evitan los errores que pueden suceder con el trabajo colaborativo.'''
+
+### Comandos GIT
+'''
+1. **git init**, empezar un proyecto de git (localmente).
+cuando tengamos un proyecto abrimos terminal y ejectuamos git init. asi inicamos git en el proyecto que nos encontramos
+
+2. **git status**, verificar cualquier cambio hecho en el proyecto (creación de nuevos archivos, editar archivos, etc.)
+'''
