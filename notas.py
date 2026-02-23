af3c3a6168fa0a1b03fce9cdc21e16c2567b335c
emedina <emedina112233@gmail.com>
2026-02-22 21:48:30 -0500

    cambios

--- notas.py
+++ notas.py
@@ -108,5 +108,7 @@ ahora debemos irnos a la rama creada:
 
 11. **git checkout**, podemos posicionarnos en cualqueir rama como también podemos omitir cambios de un archivo.
 
-git checkout featyre/cambio_en_readme
+git checkout feature/cambio_en_readme
+
+hola mundo
 '''
